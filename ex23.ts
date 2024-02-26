9ec1c2f210baae31f257d4c941c3580a4d02737e
Bisma Gondal <159830145+Bisma786@users.noreply.github.com>
2024-02-26 21:55:50 +0500

    Add files via upload

---
+++ ex23.ts
@@ -0,0 +1,31 @@
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+// let car = 'subaru';
+
+// console.log("Is car == 'subaru'? I predict True.")
+
+// console.log(car == 'subaru')
+
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+let car = 'subaru';
+
+console.log("Is car !== 'subaru'? I predict True.");
+console.log(car !== 'subaru');
+
+console.log("Is car !== 'honda'? I predict False.");
+console.log(car !== 'honda');
+
+console.log("Is car !== 'Civic'? I predict True.");
+console.log(car !== 'Civic');
+
+console.log("Is car !== 'Revo'? I predict False.");//false
+console.log(car !== 'Revo');//true
+
+let age = 25;
+console.log("Is age > 18? I predict True.");
+console.log(age>18);
+console.log("Is age <21? I predeict False.")
+console.log(age<21);
